77dd3bba1f3785409db531bee2f7b6be8b23f426
datalowe <datalowe@posteo.de>
2021-12-22 14:10:48 +0100

    add tests. raise test timeout limit

--- test/test-functional-endpoints-editorapi.js
+++ test/test-functional-endpoints-editorapi.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import { MongoMemoryServer } from 'mongodb-memory-server';
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import jwt from 'jsonwebtoken';
 
@@ -9,15 +9,26 @@ import { server } from '../dist/app.js';
 import { sendDocToCollection, createUser } from '../dist/src/db/db-functions.js';
 
 const genericUserName = 'genericUser';
-const genericUserId = 'genericId1488242kn2342';
-const genericUserName2 = 'genericUser2';
-const genericUserId2 = 'genericId2lkn2341lk';
+const genericUserPassword = 'genericPassword';
+const genericUserId = '615f496217969eef62fac69d';
+
+const genericUser = {
+  'username': genericUserName,
+  'password': genericUserPassword
+};
 
 const genericUserToken = jwt.sign(
-  { username: genericUserName, userId: genericUserId },
+  genericUser,
   process.env.JWT_SECRET,
-  { expiresIn: '1h' }
+  { expiresIn: '7d' }
 );
+
+const genericDoc = {
+  'title': 'Pocahontas',
+  'body': 'Wind, leaves and water',
+  'ownerId': genericUserId,
+  'editorIds': []
+};
 
 let mongoServer;
 
@@ -35,7 +46,7 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('invalid URL', () => {
-  it('should return a JSON object in body with error information', (done) => {
+  it('returns a JSON object in body with error information', (done) => {
     chai.request(server)
       .get('/incorrect-url')
       .end((err, res) => {
@@ -46,125 +57,89 @@ describe('invalid URL', () => {
   });
 });
 
-// describe('Editor API', () => {
-//   describe('GET /editor-api/document', () => {
-//       it(
-//         'should return an empty JSON-formatted array of documents before being populated', 
-//         (done) => {
-//           chai.request(server)
-//               .get("/editor-api/document")
-//               .set('x-access-token', genericUserToken)
-//               .end((err, res) => {
-//                   res.should.have.status(200);
-//                   res.body.should.be.an("array");
-//                   res.body.length.should.be.below(1);
-//                   done();
-//               });
-//       });
-//   });
+describe('Editor API', () => {
+  describe('GET /editor-api/graphql', () => {
+      it(
+        'returns an error if x-access-token is missing', 
+        async () => {
+          const res = await chai.request(server)
+              .get("/editor-api/graphql");
+          res.body.should.be.an('object');
+          res.body.should.have.property('authentication_error');
+      });
 
-//   describe('POST /editor-api/document', () => {
-//       it(
-//         'should return a single object representing created document', 
-//         (done) => {
-//           const testDoc = {
-//             'title': 'Pocahontas',
-//             'body': 'Wind, leaves and water',
-//             'ownerId': genericUserId,
-//             'editorIds': [genericUserId2]
-//           }
+      it(
+        'returns an error if x-access-token is invalid', 
+        async () => {
+          const res = await chai.request(server)
+              .get("/editor-api/graphql")
+              .set('x-access-token', 'invalid');
+          res.body.should.be.an('object');
+          res.body.should.have.property('authentication_error');
+      });
 
-//           chai.request(server)
-//               .post("/editor-api/document")
-//               .type('application/json')
-//               .set('x-access-token', genericUserToken)
-//               .send(testDoc)
-//               .end((err, res) => {
-//                   res.should.have.status(200);
-//                   res.body.should.be.an("object");
-//                   res.body.id.should.be.an('string');
-//                   res.body.title.should.equal(testDoc.title);
-//                   res.body.body.should.equal(testDoc.body);
+      it(
+        'returns all editors if token is valid', 
+        async () => {
+          await createUser('fauxDsn', genericUser);
+          const reqObj = {
+            query: `{
+            editors {
+                username
+                id
+            }
+          }`}
+          const reqBody = JSON.stringify(reqObj);
+          const res = await chai.request(server)
+              .get("/editor-api/graphql")
+              .send(reqBody)
+              .type('application/json')
+              .set('x-access-token', genericUserToken);
 
-//                   done();
-//               });
-//       });
-//   });
+          res.body.should.be.an('object');
+          res.body.should.have.property('data');
+          expect(res.body.data.editors.length).to.equal(1);
+      });
+  });
 
-//   describe('GET /editor-api/document/:id', () => {
-//     it(
-//       'should return object with error message if id is invalid', 
-//       (done) => {
-//         chai.request(server)
-//             .get(`/editor-api/document/4300`)
-//             .set('x-access-token', genericUserToken)
-//             .end((err, res) => {
-//                 res.body.should.be.an('object');
-//                 res.body.error.should.be.an('string');
-//                 done();
-//             });
-//     });
-//   });
+  describe("GET /editor-api/document/:id/pdf", () => {
+    it('returns an error if a nonexisting document is requested', async () => {
+      const genUserId = await createUser('fauxDsn', genericUser);
 
-//   describe('GET /editor-api/document/:id', () => {
-//     it(
-//       'should return object with corresponding id since passed id is valid', 
-//       async () => {
-//         const testDoc = {
-//           'title': 'Pocahontas',
-//           'body': 'Wind, leaves and water',
-//           'ownerId': genericUserId2,
-//           'editorIds': [genericUserId]
-//         }
-//         const generatedId = await sendDocToCollection('fauxDsn', 'editorDocs', testDoc);
+      const genUToken = jwt.sign(
+        {...genericUser, userId: genUserId},
+        process.env.JWT_SECRET,
+        { expiresIn: '7d' }
+      );
 
-//         const res = await chai.request(server)
-//           .get(`/editor-api/document/${generatedId}`)
-//           .set('x-access-token', genericUserToken);
-//         res.body.should.be.an('object');
-//         res.body.title.should.equal(testDoc.title);
-//         res.body.body.should.equal(testDoc.body);
-//     });
-//   });
+      const res = await chai.request(server)
+          .get(`/editor-api/document/1/pdf`)
+          .set('x-access-token', genUToken);
 
-//   describe('GET /editor-api/document/:id', () => {
-//     it(
-//       'should return object with error message if id is 24 chars but has no matching doc', 
-//       (done) => {
-//         chai.request(server)
-//             .get(`/editor-api/document/012345678901234567890123`)
-//             .set('x-access-token', genericUserToken)
-//             .end((err, res) => {
-//                 res.body.should.be.an('object');
-//                 res.body.error.should.be.an('string');
-//                 done();
-//             });
-//     });
-//   });
+      res.body.should.be.an('object');
+      res.body.should.have.property('error');
+    });
 
-//   describe('PUT /editor-api/document/:id', () => {
-//     it(
-//       'should return updated object with corresponding id since passed id is valid ' +
-//       'and correct new title/body are included', 
-//       async () => {
-//         const testDoc = {
-//           'title': 'Pocahontas',
-//           'body': 'Wind, leaves and water',
-//           'ownerId': genericUserId,
-//           'editorIds': [genericUserId2]
-//         }
-//         const generatedId = await sendDocToCollection('fauxDsn', 'editorDocs', testDoc);
+    it('returns a PDF if valid document is requested', async () => {
+      const genUserId = await createUser('fauxDsn', genericUser);
+      const generD = {
+        ...genericDoc,
+        ownerId: genUserId
+      }
+      const generatedId = await sendDocToCollection('fauxDsn', 'editorDocs', generD);
 
-//         testDoc.title = 'newtitle';
-//         testDoc.body = 'newbody';
-//         const res = await chai.request(server)
-//           .put(`/editor-api/document/${generatedId}`)
-//           .set('x-access-token', genericUserToken)
-//           .type('application/json')
-//           .send(testDoc);
-//         res.body.should.be.an('object');
-//         res.body.title.should.equal(testDoc.title);
-//         res.body.body.should.equal(testDoc.body);
-//     });
-//   });
-// });
+      const genUToken = jwt.sign(
+        {...genericUser, userId: genUserId},
+        process.env.JWT_SECRET,
+        { expiresIn: '7d' }
+      );
+
+      const res = await chai.request(server)
+          .get(`/editor-api/document/${generatedId}/pdf`)
+          .set('x-access-token', genUToken);
+
+      res.body.should.be.an('object');
+      expect(res).to.have.header('content-type', 'application/pdf');
+    })
+  });
+});
--- test/test-functional-endpoints-user.js
+++ test/test-functional-endpoints-user.js
@@ -3,11 +3,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import jwt from 'jsonwebtoken';
 
 import { server } from '../dist/app.js';
 import { sendDocToCollection, createUser } from '../dist/src/db/db-functions.js';
 
 let mongoServer;
+
+const genericUserName = 'genericUser';
+const genericUserId = 'genericId1488242kn2342';
 
 before(async () => {
   mongoServer = await MongoMemoryServer.create();
@@ -23,7 +27,7 @@ chai.should();
 chai.use(chaiHttp);
 
 describe('user API', () => {
-    describe('POST /user/create', () => {
+    describe('POST /user/register', () => {
       it(
         'should create a new user when username is >2 chars long and password is >5 chars long', 
         async () => {
@@ -36,6 +40,56 @@ describe('user API', () => {
             .post('/user/register')
             .type('application/json')
             .send(testUser);
+          res.body.should.be.an('object');
+          res.body.should.have.property('success');
+        }
+      );
+
+      it(
+        'produces error if request has invalid invitation token', 
+        async () => {
+          const testUser = {
+            'username': 'Pocahontas',
+            'password': 'disney'
+          };
+  
+          const res = await chai.request(server)
+            .post('/user/register')
+            .type('application/json')
+            .send({...testUser, 'invitation_code': '33'});
+          res.body.should.be.an('object');
+          res.body.should.have.property('error');
+        }
+      );
+
+      it(
+        'creates user if request has valid invitation token', 
+        async () => {
+          const testUser = {
+            'username': 'Pocahontas',
+            'password': 'disney'
+          };
+          const testDoc = {
+            'title': 'Pocahontas',
+            'body': 'Wind, leaves and water',
+            'ownerId': genericUserId,
+            'editorIds': []
+          }
+          const generatedId = await sendDocToCollection('fauxDsn', 'editorDocs', testDoc);
+          const invitationCode = jwt.sign(
+            {
+                inviterId: genericUserId,
+                inviteeEmail: 'foo@bar.com',
+                docId: generatedId
+            },
+            process.env.DOC_INVITE_SECRET,
+            { expiresIn: '7d' }
+          );
+  
+          const res = await chai.request(server)
+            .post('/user/register')
+            .type('application/json')
+            .send({...testUser, 'invitation_code': invitationCode});
           res.body.should.be.an('object');
           res.body.should.have.property('success');
         }
@@ -96,5 +150,100 @@ describe('user API', () => {
           res.body.should.have.property('token');
         }
       );
+
+      it(
+        'should return an error when invalid credentials are sent', 
+        async () => {
+          const testUser = {
+            'username': 'foo',
+            'password': 'bar'
+          };
+  
+          const res = await chai.request(server)
+            .post('/user/login')
+            .type('application/json')
+            .send(testUser);
+  
+          res.body.should.be.an('object');
+          res.body.should.have.property('error');
+        }
+      );
+
+      it(
+        'should return an error when non-existent username/password are sent', 
+        async () => {
+          const testUser = {
+            'username': 'foofoo',
+            'password': 'barbar'
+          };
+  
+          const res = await chai.request(server)
+            .post('/user/login')
+            .type('application/json')
+            .send(testUser);
+  
+          res.body.should.be.an('object');
+          res.body.should.have.property('error');
+        }
+      );
+    });
+
+    describe('GET /user/list', () => {
+      it('produces error if user has no token header',
+      async () => {
+        const testUser = {
+          'username': 'foofoo',
+          'password': 'barbar'
+        };
+
+        const res = await chai.request(server)
+          .get('/user/list')
+          .type('application/json')
+          .send(testUser);
+
+        res.body.should.be.an('object');
+        res.body.should.have.property('authentication_error');
+      });
+
+      it('produces error if user has invalid token header',
+      async () => {
+        const testUser = {
+          'username': 'foofoo',
+          'password': 'barbar'
+        };
+
+        const res = await chai.request(server)
+          .get('/user/list')
+          .set('x-access-token', 'invalid token')
+          .type('application/json')
+          .send(testUser);
+
+        res.body.should.be.an('object');
+        res.body.should.have.property('authentication_error');
+      });
+
+      it('returns all usernames if user has valid token header',
+      async () => {
+        const testUser = {
+          'username': 'Pocahontas',
+          'password': 'disney'
+        };
+
+        await createUser('fauxDsn', testUser);
+
+        const loginRes = await chai.request(server)
+          .post('/user/login')
+          .type('application/json')
+          .send(testUser);
+
+        const res = await chai.request(server)
+          .get('/user/list')
+          .set('x-access-token', loginRes.body.token)
+          .type('application/json')
+          .send(testUser);
+
+        res.body.should.be.an('object');
+        res.body.should.have.property('usernames');
+      });
     });
   });
